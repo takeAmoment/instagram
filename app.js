51705799a4927c2b71908fa55682e72d52246943
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2023-02-10 16:05:54 +0100

    init: init new project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 3000;
+mongoose.set("strictQuery", false);
+const {mongodbUri} = require("./keys");
+
+mongoose.connect(mongodbUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+    console.log("connected");
+});
+mongoose.connection.on("error", (err) => {
+    console.log(err);
+})
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
+
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongodbUri: "mongodb+srv://sasha:sasha1234@cluster1.zudjy0u.mongodb.net/?retryWrites=true&w=majority",
+};
