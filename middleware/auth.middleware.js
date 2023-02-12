658e6ab1a5e9d0f3ad97215503c0c42690bc25e3
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2023-02-12 11:22:27 +0100

    feat: create middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../keys");
+const User = require("../models/user");
+
+module.exports = async (req, res, next) => {
+    if (req.method === "OPTIONS") {
+        return next();
+    }
+
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+
+        if (!token) {
+            return res.status(401).json({message: "Unauthorized"});
+        };
+
+        const decoded = jwt.verify(token, JWT_SECRET);
+        console.log(decoded.id);
+
+        const user = await User.findOne({_id: decoded.id});
+        if (!user) {
+            return res.status(400).json({message: "Uncorrect token"});
+        };  
+        res.user = decoded;
+        next();
+
+        
+    } catch (error) {
+        res.status(401).json({message: "Unauthorized"});
+    }
+
+}
