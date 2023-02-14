f44099598f9ba49086c942620dd857d0c6bde2e4
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2023-02-14 12:21:02 +0100

    feat: change selectedKeys

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,20 +1,35 @@
 import { Header } from 'antd/es/layout/layout';
-import Menu from 'antd/es/menu';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import Menu, { MenuProps } from 'antd/es/menu';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 const items = [
+  { label: <Link to="/profile">Profile</Link>, key: 'profile' },
   { label: <Link to="/login">Login</Link>, key: 'login' },
   { label: <Link to="/register">Sign up</Link>, key: 'register' },
 ];
 const HeaderOfApp = () => {
+  const [current, setCurrent] = useState('mail');
+  const location = useLocation();
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
   return (
     <Header className={styles.header}>
       <Link to="/" className={styles.header__logo}>
         <div>Instagram</div>
       </Link>
-      <Menu theme="light" mode="horizontal" selectedKeys={[location.pathname]} items={items} />
+      <Menu
+        onClick={onClick}
+        theme="light"
+        mode="horizontal"
+        selectedKeys={[location.pathname === '/' ? location.pathname : current]}
+        items={items}
+        style={{ width: '100%', display: 'flex', justifyContent: 'end' }}
+      />
     </Header>
   );
 };
