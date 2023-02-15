65b299fd2a6974cfcbffb81e43e8617cc063b369
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2023-02-15 22:39:23 +0100

    feat: create loginApi function

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
-import { Message, RegisterInfo } from 'types/types';
+import { Message, RegisterInfo, UserData } from 'types/types';
 
 export const registerApi = async (request: RegisterInfo) =>
   await axios.post<Message>('/signup', request);
+
+export const loginApi = async (request: RegisterInfo) =>
+  await axios.post<UserData>('/signin', request);
