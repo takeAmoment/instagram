6a4f6da0149cd3e66420da89287005ec36833599
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2023-02-15 21:48:12 +0100

    feat: change PORT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const PORT = 3001;
+const PORT = 3002;
 const {mongodbUri} = require("./keys");
 
 mongoose.set("strictQuery", false);
