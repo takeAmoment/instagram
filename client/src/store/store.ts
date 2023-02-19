ee35fc222f67c3405757183578f7c15ca7c39a2f
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2023-02-19 11:31:35 +0100

    feat: add postReducer to store

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from 'features/auth.slice';
+import { postReducer } from 'features/post.slice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    post: postReducer,
   },
 });
 
