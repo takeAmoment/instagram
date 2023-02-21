cd3c637e02b59bfd4227e511f126bc20af9a5e2b
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2023-02-21 14:51:42 +0100

    feat: add unfollowApi function

--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -6,3 +6,5 @@ export const getUserApi = async () => await axios.get('/user');
 export const getUserByIdApi = async (id: string) => await axios.get(`/user/${id}`);
 
 export const followApi = async (followId: FollowId) => await axios.put(`/follow`, followId);
+
+export const unfollowApi = async (unfollowId: FollowId) => await axios.put(`/unfollow`, unfollowId);
