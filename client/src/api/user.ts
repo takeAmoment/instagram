31c8f429ef2d876934db01664598206b6caa8960
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2023-02-21 14:37:58 +0100

    feat: create getUserApi and followApi functions

--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -1,3 +1,8 @@
 import axios from 'axios';
+import { FollowId } from 'types/types';
+
+export const getUserApi = async () => await axios.get('/user');
 
 export const getUserByIdApi = async (id: string) => await axios.get(`/user/${id}`);
+
+export const followApi = async (followId: FollowId) => await axios.put(`/follow`, followId);
