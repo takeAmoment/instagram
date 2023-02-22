57be114fc0b40a1a94f83328b9d7e586b059f55e
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2023-02-22 14:51:50 +0100

    feat: create updateUserApi function

--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -8,3 +8,11 @@ export const getUserByIdApi = async (id: string) => await axios.get(`/user/${id}
 export const followApi = async (followId: FollowId) => await axios.put(`/follow`, followId);
 
 export const unfollowApi = async (unfollowId: FollowId) => await axios.put(`/unfollow`, unfollowId);
+
+export const updateUserApi = async (request: FormData) =>
+  await axios({
+    method: 'patch',
+    url: '/userupdate',
+    data: request,
+    headers: { 'Content-Type': 'multipart/form-data' },
+  });
