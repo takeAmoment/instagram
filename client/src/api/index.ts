5327016556bbbd34fc14d4efac86312d17f834df
Aliaksandra Piakhota <sasha@MacBook-Air-Aliaksandra.local>
2023-02-27 15:03:49 +0200

    feat: reload page if user unauthorized

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -21,6 +21,7 @@ axios.interceptors.response.use(
   (error) => {
     if (error.response.status == 401) {
       localStorage.clear();
+      window.location.reload();
     }
     return Promise.reject(error);
   }
